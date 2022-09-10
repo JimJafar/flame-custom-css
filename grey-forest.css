57063ba1de6e1ae2dc2a4797989bde8eeecaf3fa
Jim <jim@sangwine.net>
2022-09-10 21:55:15 +0100

    Create grey-forest.css

---
+++ grey-forest.css
@@ -0,0 +1,73 @@
+.AppCard_AppCardDetails__tbAhY {
+    height: 50%;
+}
+
+.AppCard_AppCardDetails__tbAhY span {
+    display: none !important;
+}
+
+
+.Home_SettingsButton__Qvn8C {
+    border-radius: 0.5rem !important;
+    background-color: #DFD9D6 !important;
+}
+
+
+.AppCard_AppCard__1V2_0 {
+    padding: 15px !important;
+    margin-bottom: 0 !important;
+    border-radius: 0.7rem !important;
+}
+
+
+.AppCard_AppCard__1V2_0:hover {
+    background-color: rgba(0, 0, 0, 0.3) !important;
+}
+
+@supports (-webkit-backdrop-filter: none) or (backdrop-filter: none) {
+    .AppCard_AppCard__1V2_0:hover {
+        backdrop-filter: blur(15px);
+        background-color: rgba(0, 0, 0, 0.3) !important;
+    }
+}
+
+
+body {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+}
+
+body:before {
+    content: "";
+    position: fixed;
+    overflow: hidden;
+    background-image: url("https://images.wallpaperscraft.com/image/single/trees_fog_forest_183233_2560x1440.jpg");
+    background-size: cover;
+    z-index: -999;
+    height: 100%;
+    width: 100%;
+    transform: scale(1.1);
+
+    
+    filter: blur(0.2rem);
+    opacity: 0.6;
+}
+
+
+.AppCard_AppCardIcon__8ZZTq {
+    min-width: 35px;
+}
+
+.AppCard_AppCard__1V2_0 {
+    min-width: 0
+}
+
+.AppCard_AppCardDetails__tbAhY {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+}
